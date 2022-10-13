9a4e6664b48d3fe5f768705a11db224375e1e3da
Amanda Caramico <79462423+acaramico@users.noreply.github.com>
2022-10-13 20:58:04 -0300

    update href header prof

--- IARA-APP/src/components/HearderColaborador.jsx
+++ IARA-APP/src/components/HearderColaborador.jsx
@@ -25,8 +25,8 @@ function HeaderColaborador() {
                         <img src="../img/profissionais/img-prof-1.png" alt="Foto de perfil" />
                     </button>
                     <div class="itens jflex pabsolute txt-dark-red account-menu">
-                        <a class="txt-dark-red transform-bold margin-bottom-10" href="">MINHA CONTA</a>
-                        <a class="txt-dark-red transform-bold margin-bottom-10" href="" >ANÁLISES</a>
+                        <a class="txt-dark-red transform-bold margin-bottom-10" href="/accountProfissional">MINHA CONTA</a>
+                        <a class="txt-dark-red transform-bold margin-bottom-10" href="/dashboard" >ANÁLISES</a>
                         <a class="txt-dark-red transform-bold" href="/">SAIR</a>
                     </div>
                 </div>
