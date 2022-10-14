c112a152dc28d84de7dbe517349a3c34e4b9a4d6
Amanda Caramico <79462423+acaramico@users.noreply.github.com>
2022-10-13 21:20:58 -0300

    Update CardInformacoesDoProfissional.jsx

--- IARA-APP/src/components/CardInformacoesDoProfissional.jsx
+++ IARA-APP/src/components/CardInformacoesDoProfissional.jsx
@@ -15,6 +15,7 @@ function CardInformacoesDoProfissional() {
                 </div>
                 <div class="dflex acenter jbetween">
                     <input class="input-fav" type="checkbox" id="favorite" name="favorite-checkbox" value="favorite-button"></input>
+                    {/* se o profissional já estiver favoritado adicionar propriedade defaultChecked */}
                     <label for="favorite" class="button box-shadow-none-hover shadow-smooth label-fav container bg-red txt-dark-red bg-hover-white txt-hover-dark-red margin-none margin-right-twenty">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
                     <div class="action">
