9905ab630e0f245a689bc5df9ff25c7701be0813
Amanda Caramico <79462423+acaramico@users.noreply.github.com>
2022-10-19 20:58:52 -0300

    login - alert

--- IARA-APP/src/pages/Login.jsx
+++ IARA-APP/src/pages/Login.jsx
@@ -48,6 +48,7 @@ function Login() {
             .catch(error => {
                 if (error.request.status === 401) {
                     console.log("success")
+                    alert("Email e/ou senha inválidos!")
                     swal.fire({
                         icon: "error",
                         title: <h1>Ops... Dados inválidos</h1>,
