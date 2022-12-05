f7138846134b1569a0edd8b84d0584cafbd61ee0
Lucas Menezes Teixeira <79462719+LucasTeixeira793@users.noreply.github.com>
2022-12-04 22:53:42 -0300

    fix: api.js apontando para prod

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    //baseURL:"http://44.209.44.203:8080/"
-    baseURL:"http://localhost:8080/"
+    baseURL:"http://44.209.44.203:8080/"
+    // baseURL:"http://localhost:8080/"
 })
 
 api.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
